8c747cdc9e82d66c77fcad43d51694969d5de830
Nevir Wizurai <nevwizurai@gmail.com>
2023-02-16 23:58:17 +0700

    cleaning up activity controller file

--- src/controller/activity.controller.ts
+++ src/controller/activity.controller.ts
@@ -1,52 +1,97 @@
+// Import
 import { Request, Response } from "express";
 import model from "../model/activity.model";
 import { checkValidId } from "../util/objectid";
 
+// Error catch
+const errorList = {
+  empty: {
+    code: 200,
+    message: "Empty data retrieved",
+  },
+  invalidId: {
+    code: 401,
+    message: "ID is invalid, unable to process",
+  },
+  idNotFound: {
+    code: 404,
+    message: "No registered activity with such ID",
+  },
+};
+
+// Get ALL activities
 export const getActivities = async (req: Request, res: Response): Promise<void> => {
   try {
-    const data = await model.find();
-    data.length > 0
-      ? res.status(200).json({ msg: "Get Activities success", data })
-      : res.status(200).json({ msg: "Empty data retrieved", isEmpty: true });
+    // Fetching the data
+    const data = await model.find().sort({ startAt: -1 });
+
+    // Check if data is empty
+    if (data.length === 0) throw errorList.empty;
+
+    // ...else return fetched data
+    res.status(200).json({ action: "get activities", data });
   } catch (error: any) {
-    res.status(500).json({ msg: "Get Activity failed", err: error.message });
+    // Error handling
+    res.status(error.code || 500).json({ err: error.message, action: "get activities" });
   }
 };
 
+// Add new activity
 export const addActivity = async (req: Request, res: Response): Promise<void> => {
   try {
-    const name = req.body.name as string;
-    const newData = new model({ name });
+    // Creating new model data
+    const newData = new model({ name: req.body.name });
+
+    // Saving data to database
     const data = await newData.save();
-    res.status(201).json({ msg: "Add Activity success", data });
+
+    // Returning newly added data
+    res.status(201).json({ action: "add activity", data });
   } catch (error: any) {
-    res.status(500).json({ msg: "Add Activity failed", err: error.message });
+    // Error handling
+    res.status(error.code || 500).json({ err: error.message, action: "add activity" });
   }
 };
 
+// Delete existing activity
 export const deleteActivity = async (req: Request, res: Response): Promise<void> => {
   try {
+    // Extracting and validating Id before query
     const id = req.params.id;
-    if (!checkValidId(id)) throw { message: "Tampered activity ID, unable to process" };
+    if (!checkValidId(id)) throw errorList.invalidId;
 
+    // Deleting the data by Id and store the value
     const data = await model.findByIdAndDelete(id);
-    if (!data) throw { message: "Activity with such ID does not exist" };
-    res.status(200).json({ msg: "Delete Activity success", data });
+
+    // Check if the data with provided Id is exist
+    if (!data) throw errorList.idNotFound;
+
+    // ...else return the deleted values
+    res.status(200).json({ action: "delete activity", data });
   } catch (error: any) {
-    res.status(500).json({ msg: "Delete Activity failed", err: error.message });
+    // Error handling
+    res.status(error.code || 500).json({ err: error.message, action: "delete activity" });
   }
 };
 
+// Update activity status
 export const updateActivity = async (req: Request, res: Response): Promise<void> => {
   try {
+    // Extracting and validating Id before query
     const id = req.params.id;
-    if (!checkValidId(id)) throw { message: "Tampered activity ID, unable to process" };
+    if (!checkValidId(id)) throw errorList.invalidId;
 
-    const status = req.body.status as boolean;
+    // Updating status and end time based on Id
+    const status: boolean = req.body.status;
     const data = await model.findByIdAndUpdate(id, { completed: status, endAt: Date.now() });
-    if (!data) throw { message: "Activity with such ID does not exist" };
-    res.status(200).json({ msg: "Update Activity success", data });
+
+    // Check if the data with provided Id is exist
+    if (!data) throw errorList.idNotFound;
+
+    // ...else return the deleted values
+    res.status(200).json({ action: "update activity", data });
   } catch (error: any) {
-    res.status(500).json({ msg: "Update Activity failed", err: error.message });
+    // Error handling
+    res.status(error.code || 500).json({ err: error.message, action: "update activity" });
   }
 };
